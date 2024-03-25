8ae20623f503f1eed0d5f1a4b691ab680659cc26
apgaara <138255761+apgaara@users.noreply.github.com>
2024-03-25 22:52:56 +0700

    Update config.php

--- service/config.php
+++ service/config.php
@@ -1,1 +1,14 @@
+<?php
+// Koneksi ke database
+$host = "localhost"; // Ganti sesuai dengan host Anda
+$username = "root"; // Ganti sesuai dengan username database Anda
+$password = ""; // Ganti sesuai dengan password database Anda
+$database = "login"; // Ganti sesuai dengan nama database Anda
 
+$conn = new mysqli($host, $username, $password, $database);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
